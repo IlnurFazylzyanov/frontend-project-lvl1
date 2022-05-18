4ca5a84ada021211d3521d391c1393b39076e9fd
Ilnur_Fazylzyanov <ilnurka-1989.89@mail.ru>
2022-05-18 14:16:17 +0300

    Fixed errors eslint

--- src/games/even.js
+++ src/games/even.js
@@ -6,7 +6,6 @@ import {
 } from '../index.js';
 
 const nameUser = questionAboutTheNameUser();
-
 console.log(questionAsced(0));
 
 const answerYesOrNo = (count = 2) => {
@@ -14,7 +13,8 @@ const answerYesOrNo = (count = 2) => {
   console.log(`Question: ${numberRand}`);
   const answer = answerUser();
   if ((numberRand % 2 === 0 && answer !== 'yes') || (numberRand % 2 !== 0 && answer !== 'no')) {
-    console.log('"yes" is wrong answer ;(. Correct answer was "no".');
+    const modifiedAnswer = answer === 'yes' ? 'no' : 'yes';
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${modifiedAnswer}".`);
     return `Let's try again, ${nameUser}!`;
   }
   console.log('Correct!');
