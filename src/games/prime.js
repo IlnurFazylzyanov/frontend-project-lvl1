9d88fc3b9973ac77e019cc45d589b2bd7cbac7b5
Ilnur_Fazylzyanov <ilnurka-1989.89@mail.ru>
2022-05-20 04:01:47 +0300

    Fixed a bug games brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,13 +2,11 @@ import { gameLogicFunction, numberRandom } from '../index.js';
 
 const essenceGamePrime = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const parityCheck = (number) => number % 2 === 0;
-
 const getBasicData = () => {
   const collDivisor = [];
   const numberRand = numberRandom(0, 100);
   for (let i = 2; i < numberRand; i += 1) {
-    if (parityCheck(numberRand)) {
+    if (numberRand % i === 0) {
       collDivisor.push(i);
     }
   }
